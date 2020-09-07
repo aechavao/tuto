22e49c2f9abee3db4ae999a3a755ca98f14a12d9
aechavao <aechavao@educacion.navarra.es>
2020-09-07 14:56:40 +0200

    string interpolation

--- Program.cs
+++ Program.cs
@@ -6,14 +6,26 @@ namespace tuto
     {
         static void Main(string[] args)
         {
-            if (args.Length == 0)
-            {
-                Console.WriteLine("Hello World!");
-            }
-            else
-            {
-                Console.WriteLine($"Hello {args[0]}!");
-            }
+            // https://docs.microsoft.com/es-es/dotnet/csharp/tutorials/intro-to-csharp/hello-world
+            string aFriend = "Bill";
+            Console.WriteLine(aFriend);
+            aFriend = "Maira";
+            Console.WriteLine(aFriend);
+            string firstFriend = "Maria";
+            string secondFriend = "Sage";
+            Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+            string greeting = "      Hello World!       ";
+            Console.WriteLine($"[{greeting}]");
+            string trimmedGreeting = greeting.Trim();
+            Console.WriteLine($"[{trimmedGreeting}]");
+            string songLyrics = "You say goodbye, and I say hello";
+            Console.WriteLine(songLyrics.Contains("goodbye"));
+            Console.WriteLine(songLyrics.Contains("greetings"));
+
+            //// error de inferencia //descomentar
+            //var nombre = "Luis";
+            //nombre = 3;
         }
     }
 }
