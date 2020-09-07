4dedb517c4b6ee0388ee9b13fc343a43ac3aac35
aechavao <aechavao@educacion.navarra.es>
2020-09-07 15:03:15 +0200

    div q r max min

--- Program.cs
+++ Program.cs
@@ -6,26 +6,30 @@ namespace tuto
     {
         static void Main(string[] args)
         {
-            // https://docs.microsoft.com/es-es/dotnet/csharp/tutorials/intro-to-csharp/hello-world
-            string aFriend = "Bill";
-            Console.WriteLine(aFriend);
-            aFriend = "Maira";
-            Console.WriteLine(aFriend);
-            string firstFriend = "Maria";
-            string secondFriend = "Sage";
-            Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+            // https://docs.microsoft.com/es-es/dotnet/csharp/tutorials/intro-to-csharp/numbers-in-csharp
+            int a = 7;
+            int b = 4;
+            int c = 3;
+            int d = (a + b) / c;
+            int e = (a + b) % c;
+            Console.WriteLine($"quotient: {d}");
+            Console.WriteLine($"remainder: {e}");
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"The range of integers is {min} to {max}");
+            int what = max + 3;
+            Console.WriteLine($"An example of overflow: {what}");
 
-            string greeting = "      Hello World!       ";
-            Console.WriteLine($"[{greeting}]");
-            string trimmedGreeting = greeting.Trim();
-            Console.WriteLine($"[{trimmedGreeting}]");
-            string songLyrics = "You say goodbye, and I say hello";
-            Console.WriteLine(songLyrics.Contains("goodbye"));
-            Console.WriteLine(songLyrics.Contains("greetings"));
-
-            //// error de inferencia //descomentar
-            //var nombre = "Luis";
-            //nombre = 3;
+            double da = 19;
+            double db = 23;
+            double dc = 8;
+            double dd = (da + db) / dc;
+            Console.WriteLine(dd);
+            double dmax = double.MaxValue;
+            double dmin = double.MinValue;
+            Console.WriteLine($"The range of double is {dmin} to {dmax}");
+            double third = 1.0 / 3.0;
+            Console.WriteLine(third);
         }
     }
 }
