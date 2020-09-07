e12feb90fb281a6a54624b39ee55689021bb32bf
aechavao <aechavao@educacion.navarra.es>
2020-09-07 14:36:11 +0200

    Hello World!

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tuto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
